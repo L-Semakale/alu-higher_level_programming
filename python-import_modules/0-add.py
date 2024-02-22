504068cf774803ee8f993b136093bbd9ba49e650
L-Semakale <l.semakale@alustudent.com>
2024-02-22 06:43:37 -0800

    first commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,7 +3,7 @@ from add_0.py import add
 
 a = 1
 b = 2
-print("<{} value> + <{} value> = <{} value>".format(a, b, add(a,b)))
+print("<{} > + <{} > = <{} >".format(a, b, add(a,b)))
 
-if "__main__"=="__name__"
+if "__main__"=="__name__":
     pass
