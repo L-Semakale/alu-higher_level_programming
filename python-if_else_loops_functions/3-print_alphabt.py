face29a51f0a458888f4115af57cc09e464f8182
L-Semakale <l.semakale@alustudent.com>
2024-02-22 12:31:17 -0800

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+# Using a single loop to iterate over ASCII lowercase letters
+for i in range(ord('a'), ord('z')+1):
+    # Exclude 'q' and 'e'
+    if chr(i) not in ['q', 'e']:
+        print(chr(i), end='')
