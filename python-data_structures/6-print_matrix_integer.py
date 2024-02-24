28493495511589d1ee122659780e132c58891350
L-Semakale <l.semakale@alustudent.com>
2024-02-24 13:54:22 -0800

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrates how to print a matrix of integers
+#
+# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
+# email igbinijesusamuel@gmail.com
+# -----------------------------------------------------------
+
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1 and len(matrix[0]) == 0:
-        print("")
+    """Prints a matrix of integers
+
+    Args:
+        matrix: a matrix
+    """
+
+    # If no argument is supplied print a newline
+    if not matrix:
+        print()
     else:
-        for arr in matrix:
-            for m in arr:
-                if m != arr[-1]:
-                    print("{:d}".format(m), end=" ")
+        # Loop through the rows
+        for row in matrix:
+            # Loop through the items in row
+            for item in row:
+                # Add space between items if not last item in row
+                if row.index(item) != len(row) - 1:
+                    endspace = " "
                 else:
-                    print("{:d}".format(m))
+                    endspace = ""
+                # Print the items in the row
+                print("{:d}".format(item), end=endspace)
+            print()
