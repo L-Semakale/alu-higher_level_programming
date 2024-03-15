618ea5ecab35cb39474f43b8fdf47f35f19a0307
limpho <semakalelimpho587@gmail.com>
2024-03-15 19:16:39 +0200

    11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -3,32 +3,21 @@
 more class base
 """
 
+
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 """
 Square class
 """
+
+
 class Square(Rectangle):
     """ Square Class """
     def __init__(self, size):
-        """ Initialize Square """
-        super().__init__(size, size)
+        """ size init"""
         self.__size = size
-
-    @property
-    def size(self):
-        """ Getter for size """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """ Setter for size """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value <= 0:
-            raise ValueError("size must be greater than 0")
-        self.__size = value
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        """ String representation of Square """
-        return f"[Square] {self.__size}/{self.__size}"
+        return("[Rectangle] " + str(self.__size) + "/" + str(self.__size))
