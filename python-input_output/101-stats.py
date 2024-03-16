ece526c4e9770617ab145318c38e9a30094096b3
limpho <semakalelimpho587@gmail.com>
2024-03-16 23:57:17 +0200

    7-add_item.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
-"""
-reads stdin line by line and computes metrics
+""" reads stdin line by line and computes metrics:
 """
 import sys
 
 file_size = 0
-status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-i = 0
+status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
+iter = 0
 try:
     for line in sys.stdin:
-        tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
+        iter += 1
+        line_list = line.split()
+        if len(line_list) >= 2:
+            if line_list[-2] in status_code:
+                status_code[line_list[-2]] += 1
             try:
-                file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
+                file_size += int(line_list[-1])
             except FileNotFoundError:
-                if a == i:
-                    continue
-        if i % 10 == 0:
-            print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
+                pass
+
+        if iter % 10 == 0:
+            print(f"File size: {file_size:d}")
+            for key, value in sorted(status_code.items()):
                 if value:
-                    print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
+                    print(f"{key}: {value:d}")
+
+    print(f"File size: {file_size:d}")
+    for key, value in sorted(status_code.items()):
         if value:
-            print("{:s}: {:d}".format(key, value))
+            print(f"{key}: {value:d}")
 
 except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
+    print(f"File size: {file_size:d}")
+    for key, value in sorted(status_code.items()):
         if value:
-            print("{:s}: {:d}".format(key, value))
+            print(f"{key}: {value:d}")
