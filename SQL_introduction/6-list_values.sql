b2ce8e29922eafc8c16092795192f99c0466313e
L-Semakale <l.semakale@alustudent.com>
2024-03-19 17:28:43 -0700

    6-list_values.sql

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,2 @@
--- Write a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT first_table FROM hbtn_0c_0;
-   
+-- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+SHOW TABLE first_table FROM hbtn_0c_0
