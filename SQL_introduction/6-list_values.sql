f637f092079a144b4a03497e4c5158c4f0716bce
L-Semakale <l.semakale@alustudent.com>
2024-03-19 17:39:02 -0700

    6-list_values.sql

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL Sever
-SHOW CREATE TABLE first_table FROM hbtn_0c_0
+SELECTS * FROM first_table;
