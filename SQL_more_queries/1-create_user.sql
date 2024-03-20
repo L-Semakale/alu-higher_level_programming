157fd9b205b3a339602f62ce7df662b277d56d0f
L-Semakale <l.semakale@alustudent.com>
2024-03-20 11:33:48 -0700

    1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- a script that creates the MySQL server user user_0d_1.
+CREATE USER if NOT EXISTS user_0d_1
+IDENTIFIED BY 'Secure1Pass!';
+SHOW GRANTS FOR user_0d_1
+GRANT ALL
+ON user_0d_1_pwd.*
+TO user_0d_1;
+SHOW GRANTS FOR user_0d_1;
