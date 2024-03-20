8269edf2fa6abea54c11580f79a50cc326e30c36
L-Semakale <l.semakale@alustudent.com>
2024-03-20 11:37:23 -0700

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,3 @@
--- a script that creates the MySQL server user user_0d_1.
-CREATE USER if EXISTS user_0d_1
-IDENTIFIED BY 'Secure1Pass!';
-SHOW GRANTS FOR user_0d_1
-GRANT ALL
-ON user_0d_1_pwd.*
-TO user_0d_1;
-SHOW GRANTS FOR user_0d_1;
+-- Write a script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
