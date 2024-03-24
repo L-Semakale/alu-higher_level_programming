b17030bca6165c9d38c90649c283ef53d999194c
L-Semakale <l.semakale@alustudent.com>
2024-03-24 13:35:15 +0000

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""__summary__
-- Write a Python script that takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
+"""Python script that takes in a URL and an email,
+   sends a POST request to the passed
+   URL with the email as a parameter, and
+   displays the body of the response (decoded in utf-8)
 """
+import sys
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
-    try:
-        with request.urlopen(sys.argv[1]) as resq:
-            print(resq.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print('Error code:', e.code)
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
