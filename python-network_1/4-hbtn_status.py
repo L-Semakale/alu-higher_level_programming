4115fec00cd7bc09730d5261240d9fe4a7f1ec1d
L-Semakale <l.semakale@alustudent.com>
2024-03-24 19:18:34 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-Fetches the status from the given URL and displays the response body.
-"""
+# python script that fetches 'https://alu-intranet.hbtn.io/status'
+""" fetch 'https://intranet.hbtn.io/status'"""
 import requests
 
+
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    response = requests.get(url)
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
