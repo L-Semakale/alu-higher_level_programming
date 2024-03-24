8fcd7fa458437508ab93868a4f4be276e03fd716
L-Semakale <l.semakale@alustudent.com>
2024-03-24 19:21:03 +0000

    5-hbtn_header.py

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+# python script that takes in a URL,
+# sends a request to the URL and displays the value of the,
+# variable X-Request-Id in the response header
 """
-    Script that takes in a URL, sends a request to the URL and dsiplays
-    value of the variable X-Request-Id
+    send a request to the URL & display value of the,
+    variable X-Request-Id in the response header
 """
-import requests
 import sys
+import requests
 
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    print("{}".format(r.headers['X-Request-Id']))
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
