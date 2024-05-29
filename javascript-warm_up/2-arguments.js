6b612aa5035122ff10b916393d955887e7af563e
limpho <semakalelimpho587@gmail.com>
2024-05-29 19:23:56 +0200

    Javascript warm up

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-
-switch (args.length) {
-    case 0:
-        console.log('No argument');
-        break;
-    case 1:
-        console.log('Argument found');
-        break;
-    default:
-        console.log('Arguments found');
-        break;
+if (args.length === 0) {
+    console.log('No argument');
+} else if (args.length === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
 }
